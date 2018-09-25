063d8e00d1398f1b61ce12a8b776272063d6168f
Swethasarathy <40590670+Swethasarathy@users.noreply.github.com>
2018-09-25 09:40:09 +0530

    Create WebServiceClient.java

---
+++ WebServiceClient.java
@@ -0,0 +1,14 @@
+package webservice.client;
+
+public class WebServiceClient 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+    
+}
